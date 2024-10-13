faecf54df957544c440e209817eea0ea873a16e2
Krish Singh <singhkrish4u@gmail.com>
2024-10-13 16:06:45 -0500

    Compiled motor and color detection functionality w/ modes

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -12,6 +12,9 @@
 #include <Bluepad32.h>
 #include <ESP32Servo.h>
 #include <bits/stdc++.h>
+#include <ESP32SharpIR.h>
+#include <QTRSensors.h>
+#include <Arduino_APDS9960.h>
 
 
 #define IN1 4
@@ -20,9 +23,18 @@
 #define IN3 5
 #define IN4 18
 #define ENB 19
+#define APDS9960_INT 2
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define I2C_FREQ 100000
+
+TwoWire I2C_0 = TwoWire(0);
+APDS9960 apds = APDS9960(I2C_0, APDS9960_INT);
+
 int joyX;
 int joyY;
-//Servo servo;
+int mode = 1;
+Servo servo;
 
 GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
 
@@ -92,6 +104,24 @@ void leftMotor(int speed){
     //Serial.println(speed);
 }
 
+void colordetect() {
+    int r, g, b, a;
+    // Wait until color is read from the sensor 
+    while (!apds.colorAvailable()) { delay(5); }
+    apds.readColor(r, g, b, a);
+    // Read color from sensor apds.readColor(r, g, b, a);
+    // Print color in decimal 
+    Serial.print("RED: ");
+    Serial.print(r);
+    Serial.print(" GREEN: ");
+    Serial.print(g);
+    Serial.print(" BLUE: ");
+    Serial.print(b);
+    Serial.print(" AMBIENT: ");
+    Serial.println(a);
+    delay(100);
+}
+
 void setup() {
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
     BP32.forgetBluetoothKeys();
@@ -105,6 +135,13 @@ void setup() {
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
     pinMode(ENB, OUTPUT);
+
+    //sets up I2C protocol; no need to worry about how it works, just used to pull info from the color sensor
+    I2C_0.begin(I2C_SDA, I2C_SCL, I2C_FREQ);
+
+    //sets up color sensor
+    apds.setInterruptPin(APDS9960_INT);
+    apds.begin();
    
     Serial.begin(115200);
     joyX = 0;
@@ -117,13 +154,28 @@ void loop() {
         GamepadPtr controller = myGamepads[i];
         if (controller && controller->isConnected()) {
             if (controller->b()) {
-                Serial.println("button a pressed");
+                Serial.println("button A pressed");
+                mode = 1;
             }
+
+            if (controller->a()) {
+                Serial.println("button B pressed");
+                mode = 2;
+            }
+
+            if (mode == 1) { //manual control 
             joyX = controller->axisX() / 3;
             joyY = controller->axisY() / 3;
             rightMotor(joyY+joyX);
             leftMotor(joyY-joyX);
-           /*if (controller->l1() == 1) {
+            }
+
+            if (mode == 2) { //colordetect mode as placeholder to autonomous stage 1
+                colordetect();
+            }
+
+
+           if (controller->l1() == 1) {
                 Serial.print("Servo move");
                 servo.write(1000);
             }
@@ -132,7 +184,7 @@ void loop() {
                 servo.write(1500);
             }
 
-            if(controller->axisRY() > 0) { // negative y is upward on stick
+           /* if(controller->axisRY() > 0) { // negative y is upward on stick
                 Serial.println(" DC motor move");
                 digitalWrite(IN1, LOW);
                 digitalWrite(IN2, HIGH);
@@ -142,8 +194,6 @@ void loop() {
                 digitalWrite(IN1, LOW);
                 digitalWrite(IN2, LOW);
             }*/
-
-            // PHYSICAL BUTTON A
 
         }
         vTaskDelay(1);
